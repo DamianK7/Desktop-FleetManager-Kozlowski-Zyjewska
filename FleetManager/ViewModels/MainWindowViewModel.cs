b245c935abebf3bbedd4089f722763a25b99144b
UlaRzyj <urszula.zyjewska@cyfroweszkoly.pl>
2026-03-18 21:24:30 +0100

    load vehicles

--- FleetManager/ViewModels/MainWindowViewModel.cs
+++ FleetManager/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using FleetManager.Models;
 using FleetManager.Services;
 
@@ -13,5 +14,19 @@ public class MainWindowViewModel : ViewModelBase
     public MainWindowViewModel(IVehicleService vehicleService)
     {
         _vehicleService = vehicleService;
+
+        Task.Run(async () => await LoadAsync());
+    }
+
+
+    private async Task LoadAsync()
+    {
+        var list = await _vehicleService.LoadAsync();
+        
+        Vehicles.Clear();
+        foreach (var v in list)
+        {
+            Vehicles.Add(v);
+        }
     }
 }
