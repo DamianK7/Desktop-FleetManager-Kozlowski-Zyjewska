935ad68894c4d759bccf0b4daec61702724cf145
UlaRzyj <urszula.zyjewska@cyfroweszkoly.pl>
2026-03-18 21:36:56 +0100

    vehicle status

--- FleetManager/ViewModels/MainWindowViewModel.cs
+++ FleetManager/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.ObjectModel;
+using System.Reactive;
 using System.Threading.Tasks;
 using FleetManager.Models;
 using FleetManager.Services;
+using ReactiveUI;
 
 namespace FleetManager.ViewModels;
 
@@ -10,6 +12,10 @@ public class MainWindowViewModel : ViewModelBase
     public ObservableCollection<Vehicle> Vehicles { get; } = new();
 
     private readonly IVehicleService _vehicleService;
+    
+    public ReactiveCommand<Vehicle, Unit> RefuelCommand { get; }
+    public ReactiveCommand<Vehicle, Unit> SendToRouteCommand { get; }
+    public ReactiveCommand<Vehicle, Unit> SetServiceCommand { get; }
 
     public MainWindowViewModel(IVehicleService vehicleService)
     {
@@ -28,5 +34,30 @@ public class MainWindowViewModel : ViewModelBase
         {
             Vehicles.Add(v);
         }
+    }
+
+    private async Task SaveAsync()
+    {
+        await _vehicleService.SaveAsync(Vehicles);
+    }
+
+    private void Refuel(Vehicle v)
+    {
+        if (v.Status == VehicleStatus.InRoute)
+            return;
+        v.Fuel = 100;
+    }
+
+    private void SendToRoute(Vehicle v)
+    {
+        if (v.Fuel < 15 || v.Status == VehicleStatus.Service)
+            return;
+        
+        v.Status = VehicleStatus.InRoute;
+    }
+
+    private void SetService(Vehicle v)
+    {
+        v.Status = VehicleStatus.Service
     }
 }
