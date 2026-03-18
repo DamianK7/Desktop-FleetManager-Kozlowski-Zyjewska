584a52fb56f3583a55aad7584029a975b35a0078
UlaRzyj <urszula.zyjewska@cyfroweszkoly.pl>
2026-03-18 21:03:09 +0100

    add vehicle model

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -1,6 +1,11 @@
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
 namespace FleetManager.Models;
 
 public class Vehicle
 {
-    
+    public string Name { get; set; } = "";
+    public int Fuel { get; set; }
+    public VehicleStatus Status { get; set; }
 }
