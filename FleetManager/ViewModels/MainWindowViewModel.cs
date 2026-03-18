777d2978d7b469856c10b24f817abc3f74ae8252
UlaRzyj <urszula.zyjewska@cyfroweszkoly.pl>
2026-03-18 21:46:28 +0100

    update vehiclestatus

--- FleetManager/ViewModels/MainWindowViewModel.cs
+++ FleetManager/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Reactive;
 using System.Threading.Tasks;
 using FleetManager.Models;
@@ -48,20 +49,36 @@ public class MainWindowViewModel : ViewModelBase
     private void Refuel(Vehicle v)
     {
         if (v.Status == VehicleStatus.InRoute)
+        {
+            Console.WriteLine("Nie można zatankować w trasie");
             return;
+        }
+            
         v.Fuel = 100;
+        _ = SaveAsync();
     }
 
     private void SendToRoute(Vehicle v)
     {
         if (v.Fuel < 15 || v.Status == VehicleStatus.Service)
+        {
+            Console.WriteLine("Nie można wysłać w trasę");
             return;
+        } if (v.Status == VehicleStatus.Service)
+        {
+            Console.WriteLine("Nie można wysłać w trasę");
+            return;
+        }
+            
         
         v.Status = VehicleStatus.InRoute;
+        
+        _ = SaveAsync();
     }
 
     private void SetService(Vehicle v)
     {
-        v.Status = VehicleStatus.Service
+        v.Status = VehicleStatus.Service;
+        _ = SaveAsync();
     }
 }
